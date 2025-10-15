68f9dcae53950f4fddeb64f41f3600217e21a780
Javier Uncos <javiersrfc30@gmail.com>
2025-10-15 17:26:50 -0300

    agregamos seccion de cards

---
+++ src/components/sections/SectionCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionCards = () => {
+    return (
+        <div className='h-[200px] bg-amber-600'>
+            
+        </div>
+    );
+};
+
+export default SectionCards;
