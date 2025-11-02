271f04a0ee7eaf2593a65a5611894677570e4b84
Javier Uncos <javiersrfc30@gmail.com>
2025-11-02 15:52:19 -0300

    cambia de ruta de productos en categoria mas vandidos

--- src/components/ui/CardMasVendidos.jsx
+++ src/components/ui/CardMasVendidos.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CardMasVendidos = ({ producto }) => {
   return (
     <>
-      <a href={`/productos/${producto.id}`}>
+      <a href={`/detalle/${producto.id}`}>
         <div className="group w-full bg-white border border-gray-200 rounded-md overflow-hidden transition-all duration-500 flex flex-col md:flex-row items-center md:items-start gap-6 p-4 md:p-6 h-auto pb-10 md:h-[260px]">
           {/* Imagen */}
           <div className="flex-shrink-0 w-full md:w-[230px] h-[300px] md:h-[200px] flex justify-center items-center bg-[#191919] rounded-2xl overflow-hidden">
